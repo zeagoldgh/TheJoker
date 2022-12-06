149b98c7db02f90afe57998b9f109d1902d4c778
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-12-06 10:47:35 +0100

    modified main.py code

---
+++ infrastructure/joke_crawler.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "joke_crawler" {
+  function_name = "joke_crawler"
+  s3_bucket     = "joke-src-bucket-dzaa1417"
+  s3_key        = "joke-crawler.zip"
+  role          = "arn:aws:iam::208631683274:role/LabRole"
+  handler    = "crawler.handler"
+  timeout       = 300
+  runtime       = "python3.9"
+  layer         =[aws_lambda_layer_version.requests_layer.arn]
+}
+
+resource "aws_lambda_layer_version" "requests_layer" {
+  s3_bucket     = "joke-src-bucket-dzaa1417"
+  s3_key        = "requests-layer.zip"
+  layer_name    = "requests-layer"
+
+  compatible_runtimes = ["python3.9"]
+}
