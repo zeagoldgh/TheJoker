3055ee1b114b8f5b77f7b58b2537443345bc4509
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-12-07 09:47:00 +0100

    cloud watch was add to joke_crawler

--- infrastructure/joke_crawler.tf
+++ infrastructure/joke_crawler.tf
@@ -16,3 +16,23 @@ resource "aws_lambda_layer_version" "requests_layer" {
 
   compatible_runtimes = ["python3.9"]
 }
+
+resource "aws_cloudwatch_event_rule" "every_seven_minutes" {
+    name = "every_seven_minutes"
+    description = "Fires every_seven_minutes"
+    schedule_expression = "rate(7 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "joke_crawler_every_seven_minutes" {
+    rule = aws_cloudwatch_event_rule.every_seven_minutes.name
+    target_id = "crawled-jokes"
+    arn = aws_lambda_function.joke_crawler.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_joke_crawler" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.joke_crawler.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_seven_minutes.arn
+}
