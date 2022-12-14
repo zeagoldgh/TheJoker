aea1b6f6f05885823a80f97fc7d528d0f411e347
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-12-14 10:39:52 +0100

    few changes

--- joke-data-crawler/src/dynamodb_crawler.py
+++ joke-data-crawler/src/dynamodb_crawler.py
@@ -1,4 +1,4 @@
-from chucknorrisapi import load_jokes
+from chucknorrisapi import load_joke
 from dynamodb_utils import save_joke_to_dynamodb
 
 
@@ -11,9 +11,9 @@ def map_jokes(joke_from_api):
         })
 
 def handler(event, context):
-    jokes_from_api = load_jokes()
-    jokes = map_jokes(jokes_from_api)
-    save_joke_to_dynamodb(jokes)
+    joke_from_api = load_joke()
+    joke = map_jokes(joke_from_api)
+    save_joke_to_dynamodb(joke)
     
 
 if __name__ == "__main__":
