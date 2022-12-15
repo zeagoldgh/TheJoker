ea60586b6b8abe66675a7ce6c44da80a0f3a2d9a
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-12-15 10:45:42 +0100

    joke endpoint was added

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 def health():
     return {"health": "OK"}
+
+@app.get("/joke")
+def getJokes():
+    return [{
+        "id": "id1",
+        "value": "Hilarious Joke"
+    },{
+        "id": "id2",
+        "value": "Funny Joke"
+    }]
